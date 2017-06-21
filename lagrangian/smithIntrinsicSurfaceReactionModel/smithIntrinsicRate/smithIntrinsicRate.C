34fc8c19547cc7368a8b12b7e41f54a07d97d242
Clint Dunn <cdunn6754@gmail.com>
2017-06-21 13:05:48 -0600

    finished with smithIntrinsic now starting testing

--- lagrangian/smithIntrinsicSurfaceReactionModel/smithIntrinsicRate/smithIntrinsicRate.C
+++ lagrangian/smithIntrinsicSurfaceReactionModel/smithIntrinsicRate/smithIntrinsicRate.C
@@ -37,21 +37,23 @@ Foam::smithIntrinsicRate<CloudType>::smithIntrinsicRate
     CloudType& owner
 )
 :
-    SurfaceReactionModel<CloudType>(dict, owner, typeName),
-    Sb_(readScalar(this->coeffDict().lookup("Sb"))),
-    C1_(readScalar(this->coeffDict().lookup("C1"))),
-    rMean_(readScalar(this->coeffDict().lookup("rMean"))),
-    theta_(readScalar(this->coeffDict().lookup("theta"))),
-    Ai_(readScalar(this->coeffDict().lookup("Ai"))),
-    Ei_(readScalar(this->coeffDict().lookup("Ei"))),
-    Ag_(readScalar(this->coeffDict().lookup("Ag"))),
-    tau_(this->coeffDict().lookupOrDefault("tau", sqrt(2.0))),
-    CsLocalId_(-1),
-    O2GlobalId_(owner.composition().carrierId("O2")),
-    CO2GlobalId_(owner.composition().carrierId("CO2")),
-    WC_(0.0),
-    WO2_(0.0),
-    HcCO2_(0.0)
+  SurfaceReactionModel<CloudType>(dict, owner, typeName),
+  Sb_(this->coeffDict().lookupOrDefault("Sb", 1.33)),
+  Cdiff_(this->coeffDict().lookupOrDefault("Cdiff", 5.e-12)),
+  rMean_(this->coeffDict().lookupOrDefault("rMean", 6.e-8)),
+  theta_(this->coeffDict().lookupOrDefault("theta",0.1)),
+  Ai_(this->coeffDict().lookupOrDefault("Ai", 0.052)),
+  Ei_(this->coeffDict().lookupOrDefault("Ei", 161.5)),
+  Ag_(this->coeffDict().lookupOrDefault("Ag", 104.)),
+  zeta_(this->coeffDict().lookupOrDefault("zeta", sqrt(2.0))),
+  D0_(this->coeffDict().lookupOrDefault("D0", 3.13e-4)),
+  Ckn_(this->coeffDict().lookupOrDefault("Ckn", 97.0)),
+  CsLocalId_(-1),
+  O2GlobalId_(owner.composition().carrierId("O2")),
+  CO2GlobalId_(owner.composition().carrierId("CO2")),
+  WC_(0.0),
+  WO2_(0.0),
+  HcCO2_(0.0)
 {
     // Determine Cs ids
     label idSolid = owner.composition().idSolid();
@@ -85,13 +87,15 @@ Foam::smithIntrinsicRate<CloudType>::smithIntrinsicRate
 :
     SurfaceReactionModel<CloudType>(srm),
     Sb_(srm.Sb_),
-    C1_(srm.C1_),
+    Cdiff_(srm.Cdiff_),
     rMean_(srm.rMean_),
     theta_(srm.theta_),
     Ai_(srm.Ai_),
     Ei_(srm.Ei_),
     Ag_(srm.Ag_),
-    tau_(srm.tau_),
+    zeta_(srm.zeta_),
+    D0_(srm.D0_),
+    Ckn_(srm.Ckn_),
     CsLocalId_(srm.CsLocalId_),
     O2GlobalId_(srm.O2GlobalId_),
     CO2GlobalId_(srm.CO2GlobalId_),
@@ -154,42 +158,48 @@ Foam::scalar Foam::smithIntrinsicRate<CloudType>::calculate
         return 0.0;
     }
 
+    // Average temperature between particle and gas 
+    const scalar Tm = 0.5*(T + Tc);
+
     // Diffusion rate coefficient [m2/s]
-    const scalar D0 = C1_/d*pow(0.5*(T + Tc), 0.75);
+    const scalar Rox = Cdiff_/d*pow(Tm, 0.75);
 
     // Apparent density of pyrolysis char [kg/m3]
     const scalar rhop = 6.0*mass/(constant::mathematical::pi*pow3(d));
 
     // Knusden diffusion coefficient [m2/s]
-    const scalar Dkn = 97.0*rMean_*sqrt(T/WO2_);
+    const scalar Dkn = Ckn_*rMean_*sqrt(T/WO2_);
+
+    // Reference temperature for the oxygen diffusion [K]
+    const scalar T0 = 1500 ;
+
+    // Oxygen diffusion coefficient [m2/s]
+    const scalar Dox = D0_*pow(Tm/T0, 1.75);
 
     // Effective diffusion [m2/s]
-    const scalar De = theta_/sqr(tau_)/(1.0/Dkn + 1/D0);
-
-    // Cell carrier phase O2 species density [kg/m^3]
-    const scalar rhoO2 = rhoc*YO2;
-
-    // Partial pressure O2 [Pa]
-    const scalar ppO2 = rhoO2/WO2_*RR*Tc;
+    const scalar De = theta_/sqr(zeta_)/(1.0/Dkn + 1/Dox);
 
     // Intrinsic reactivity [1/s]
-    const scalar ki = Ai_*exp(-Ei_/RR/T);
+    const scalar ki = Ai_*exp(-(Ei_*1000)/(RR/1000.0)/T);
 
     // Thiele modulus []
     const scalar phi =
-        max(0.5*d*sqrt(Sb_*rhop*Ag_*ki*ppO2/(De*rhoO2)), ROOTVSMALL);
+      max(0.5*d*sqrt(Sb_*rhop*(Ag_*1000.)*ki*pc/(De*rhoc)), ROOTVSMALL);
+
+    Info << "Is this the pressure? " << pc 
+	 << " and RR is (8000?): " << RR <<  endl;
 
     // Effectiveness factor []
-    const scalar eta = max(3.0*sqr(phi)*(phi/tanh(phi) - 1.0), 0.0);
+    const scalar eta = max(3.0/sqr(phi)*(phi/tanh(phi) - 1.0), 0.0);
 
     // Chemical rate [kmol/m2/s]
-    const scalar R = eta*d/6.0*rhop*Ag_*ki;
+    const scalar Rch = eta*d/6.0*rhop*(Ag_*1000.)*ki;
 
     // Particle surface area [m2]
     const scalar Ap = constant::mathematical::pi*sqr(d);
 
     // Change in C mass [kg]
-    scalar dmC = Ap*rhoc*RR*Tc*YO2/WO2_*D0*R/(D0 + R)*dt;
+    scalar dmC = Ap*pc*YO2*Rox*Rch/(Rox + Rch)*dt;
 
     // Limit mass transfer by availability of C
     dmC = min(mass*Ychar, dmC);
